549e3a75311631cf262a0de807ea62bfa1e7dc34
Braulio Diez Botella <braulio.diez@gmail.com>
2023-12-17 23:10:56 +0100

    wip

--- src/pods/canvas/canvas.pod.tsx
+++ src/pods/canvas/canvas.pod.tsx
@@ -3,7 +3,14 @@ import classes from "./canvas.pod.module.css";
 export const CanvasPod: React.FC = () => {
   return (
     <div className={classes.container}>
-      <h1>Canvas Pod</h1>
+      <svg
+        viewBox="0 0 100 100"
+        style={{ border: "1px solid black" }}
+        className={classes.containerSvg}
+      >
+        <rect x={0} y={0} width={10} height={15} fill="green" />
+        <circle cx={40} cy={35} r={25} fill="red" />
+      </svg>
     </div>
   );
 };
