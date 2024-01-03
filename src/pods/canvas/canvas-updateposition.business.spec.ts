4bc2783e039aa5a829b9c38e8e4e951b6a96e9b4
LeticiadelaOsa <deletidev@gmail.com>
2024-01-03 12:30:52 +0100

    Update import

--- src/pods/canvas/canvas-updateposition.business.spec.ts
+++ src/pods/canvas/canvas-updateposition.business.spec.ts
@@ -1,6 +1,6 @@
 import { Size } from '@/core/model';
-import { UpdateInfo, calculateTablePosition } from './canvas.business';
-import { DatabaseSchemaVm, RelationVm, TableVm } from './canvas.vm';
+import { calculateTablePosition } from './canvas.business';
+import { DatabaseSchemaVm, RelationVm, TableVm, UpdateInfo } from './canvas.vm';
 
 describe('calculateTablePosition', () => {
   it('updates the position of the specified table within canvas boundaries', () => {
