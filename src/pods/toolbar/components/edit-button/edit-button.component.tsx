4e5d5470f7c413c3ba9aa790b0865da33a0189ca
MTeresaMB <milanesteresa@gmail.com>
2024-01-08 13:37:02 +0100

    fixed errors conflicts and add color to relation line

--- src/pods/toolbar/components/edit-button/edit-button.component.tsx
+++ src/pods/toolbar/components/edit-button/edit-button.component.tsx
@@ -3,12 +3,30 @@ import { EditTablePod } from '@/pods/edit-table';
 import { ToolbarButton } from '../toolbar-button/toolbarButton.component';
 import { Edit } from '@/common/components/icons';
 import classes from '@/pods/toolbar/toolbar.pod.module.css';
+import {
+  TableVm,
+  useCanvasSchemaContext,
+} from '@/core/providers/canvas-schema';
 
 export const EditButton = () => {
-  const { openModal } = useModalDialogContext();
-
+  const { openModal, closeModal } = useModalDialogContext();
+  const { canvasSchema, addTable } = useCanvasSchemaContext();
+  const handleAddTable = (newTable: TableVm) => {
+    // TODO: Calculate X,Y position based on canvas current view port, maybe we have
+    // to keep this info in the settings or new context, then update newTable object X,Y position
+    // #62
+    // https://github.com/Lemoncode/mongo-modeler/issues/62
+    addTable(newTable);
+    closeModal();
+  };
   const handleEditTableClick = () => {
-    openModal(<EditTablePod />);
+    openModal(
+      <EditTablePod
+        relations={canvasSchema.relations}
+        onSave={handleAddTable}
+      />,
+      true
+    );
   };
   return (
     <ToolbarButton
