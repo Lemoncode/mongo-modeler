91f3c09fdab1ae396862c10319d6b2390fe30476
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2024-01-09 16:03:28 +0100

    work in progress

--- src/core/providers/canvas-schema/canvas-schema.business.spec.ts
+++ src/core/providers/canvas-schema/canvas-schema.business.spec.ts
@@ -4,7 +4,7 @@ import { updateTable } from './canvas-schema.business';
 
 describe('canvas-schema.business', () => {
   describe('updateTable', () => {
-    it('Should updated one table field when the id table its the same', () => {
+    it('Should updated one table field when the id table schema its the same', () => {
       // Arrange
       const table: TableVm = {
         id: '1',
@@ -174,7 +174,7 @@ describe('canvas-schema.business', () => {
       expect(result).toEqual(expectedResult);
     });
 
-    it('Should remove all the tables when the id table its the same', () => {
+    it('Should remove all the table fields when the id table schema its the same', () => {
       // Arrange
       const table: TableVm = {
         id: '1',
@@ -239,7 +239,7 @@ describe('canvas-schema.business', () => {
       expect(result).toEqual(expectedResult);
     });
 
-    it('Should remove only one table when the id table its the same', () => {
+    it('Should remove only one table fields when the id table schema its the same', () => {
       // Arrange
       const table: TableVm = {
         id: '1',
@@ -332,6 +332,161 @@ describe('canvas-schema.business', () => {
             tableName: 'test update table name',
             x: 20,
             y: 6,
+          },
+        ],
+      };
+      // Act
+      const result = updateTable(table, dbSchema);
+
+      // Assert
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('Should update one tables when the id table schemas is the same but the another one no', () => {
+      // Arrange
+      const table: TableVm = {
+        id: '1',
+        fields: [
+          {
+            id: '1',
+            name: 'test update id name 1',
+            PK: true,
+            type: 'objectId',
+          },
+          {
+            id: '2',
+            name: 'test update id name 2',
+            PK: false,
+            type: 'number',
+          },
+        ],
+        tableName: 'test update table name',
+        x: 20,
+        y: 6,
+      };
+
+      const dbSchema: DatabaseSchemaVm = {
+        relations: [
+          {
+            fromFieldId: '10',
+            fromTableId: '9',
+            toFieldId: '1',
+            toTableId: '1',
+            type: '1:M',
+          },
+        ],
+        tables: [
+          {
+            id: '1',
+            fields: [
+              {
+                id: '1',
+                name: 'test not update name',
+                PK: true,
+                type: 'objectId',
+              },
+              {
+                id: '2',
+                name: 'test update id name 2',
+                PK: false,
+                type: 'string',
+              },
+              {
+                id: '3',
+                name: 'test update id name 3',
+                PK: false,
+                type: 'object',
+              },
+            ],
+            tableName: 'test table name',
+            x: 5,
+            y: 15,
+          },
+          {
+            id: '2',
+            fields: [
+              {
+                id: '1',
+                name: 'test not update name',
+                PK: true,
+                type: 'objectId',
+              },
+              {
+                id: '2',
+                name: 'test update id name 2',
+                PK: false,
+                type: 'string',
+              },
+              {
+                id: '3',
+                name: 'test update id name 3',
+                PK: false,
+                type: 'object',
+              },
+            ],
+            tableName: 'test table 2 name',
+            x: 5,
+            y: 15,
+          },
+        ],
+      };
+
+      const expectedResult: DatabaseSchemaVm = {
+        relations: [
+          {
+            fromFieldId: '10',
+            fromTableId: '9',
+            toFieldId: '1',
+            toTableId: '1',
+            type: '1:M',
+          },
+        ],
+        tables: [
+          {
+            id: '1',
+            fields: [
+              {
+                id: '1',
+                name: 'test update id name 1',
+                PK: true,
+                type: 'objectId',
+              },
+              {
+                id: '2',
+                name: 'test update id name 2',
+                PK: false,
+                type: 'number',
+              },
+            ],
+            tableName: 'test update table name',
+            x: 20,
+            y: 6,
+          },
+          {
+            id: '2',
+            fields: [
+              {
+                id: '1',
+                name: 'test not update name',
+                PK: true,
+                type: 'objectId',
+              },
+              {
+                id: '2',
+                name: 'test update id name 2',
+                PK: false,
+                type: 'string',
+              },
+              {
+                id: '3',
+                name: 'test update id name 3',
+                PK: false,
+                type: 'object',
+              },
+            ],
+            tableName: 'test table 2 name',
+            x: 5,
+            y: 15,
           },
         ],
       };
