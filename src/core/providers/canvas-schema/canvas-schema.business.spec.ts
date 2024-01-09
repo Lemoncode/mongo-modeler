0569f331f53b241a5ccf2a790295f923c025279e
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2024-01-09 14:16:58 +0100

    add more test

--- src/core/providers/canvas-schema/canvas-schema.business.spec.ts
+++ src/core/providers/canvas-schema/canvas-schema.business.spec.ts
@@ -4,21 +4,21 @@ import { updateTable } from './canvas-schema.business';
 
 describe('canvas-schema.business', () => {
   describe('updateTable', () => {
-    it('', () => {
+    it('Should updated one table field when the ids table its the same', () => {
       // Arrange
       const table: TableVm = {
         id: '1',
         fields: [
           {
             id: '1',
-            name: 'test name',
+            name: 'test update id name',
             PK: true,
             type: 'objectId',
           },
         ],
-        tableName: 'test table name',
-        x: 20,
-        y: 6,
+        tableName: 'test update table name',
+        x: 10,
+        y: 2,
       };
 
       const dbSchema: DatabaseSchemaVm = {
@@ -65,14 +65,184 @@ describe('canvas-schema.business', () => {
             fields: [
               {
                 id: '1',
+                name: 'test update id name',
+                PK: true,
+                type: 'objectId',
+              },
+            ],
+            tableName: 'test update table name',
+            x: 10,
+            y: 2,
+          },
+        ],
+      };
+      // Act
+      const result = updateTable(table, dbSchema);
+
+      // Assert
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('Should updated two table fields when the ids table its the same', () => {
+      // Arrange
+      const table: TableVm = {
+        id: '1',
+        fields: [
+          {
+            id: '1',
+            name: 'test update id name 1',
+            PK: true,
+            type: 'objectId',
+          },
+          {
+            id: '2',
+            name: 'test update id name 2',
+            PK: false,
+            type: 'number',
+          },
+        ],
+        tableName: 'test update table name',
+        x: 20,
+        y: 6,
+      };
+
+      const dbSchema: DatabaseSchemaVm = {
+        relations: [
+          {
+            fromFieldId: '10',
+            fromTableId: '9',
+            toFieldId: '1',
+            toTableId: '1',
+            type: '1:M',
+          },
+        ],
+        tables: [
+          {
+            id: '1',
+            fields: [
+              {
+                id: '1',
+                name: 'test not update name',
+                PK: true,
+                type: 'objectId',
+              },
+            ],
+            tableName: 'test table name',
+            x: 5,
+            y: 15,
+          },
+        ],
+      };
+
+      const expectedResult: DatabaseSchemaVm = {
+        relations: [
+          {
+            fromFieldId: '10',
+            fromTableId: '9',
+            toFieldId: '1',
+            toTableId: '1',
+            type: '1:M',
+          },
+        ],
+        tables: [
+          {
+            id: '1',
+            fields: [
+              {
+                id: '1',
+                name: 'test update id name 1',
+                PK: true,
+                type: 'objectId',
+              },
+              {
+                id: '2',
+                name: 'test update id name 2',
+                PK: false,
+                type: 'number',
+              },
+            ],
+            tableName: 'test update table name',
+            x: 20,
+            y: 6,
+          },
+        ],
+      };
+      // Act
+      const result = updateTable(table, dbSchema);
+
+      // Assert
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('Not should update table fields when the ids table it is not the same', () => {
+      // Arrange
+      const table: TableVm = {
+        id: '2',
+        fields: [
+          {
+            id: '1',
+            name: 'test update id name',
+            PK: true,
+            type: 'objectId',
+          },
+        ],
+        tableName: 'test update table name',
+        x: 7,
+        y: 20,
+      };
+
+      const dbSchema: DatabaseSchemaVm = {
+        relations: [
+          {
+            fromFieldId: '10',
+            fromTableId: '9',
+            toFieldId: '1',
+            toTableId: '1',
+            type: '1:1',
+          },
+        ],
+        tables: [
+          {
+            id: '1',
+            fields: [
+              {
+                id: '1',
                 name: 'test name',
                 PK: true,
                 type: 'objectId',
               },
             ],
             tableName: 'test table name',
-            x: 20,
-            y: 6,
+            x: 15,
+            y: 25,
+          },
+        ],
+      };
+
+      const expectedResult: DatabaseSchemaVm = {
+        relations: [
+          {
+            fromFieldId: '10',
+            fromTableId: '9',
+            toFieldId: '1',
+            toTableId: '1',
+            type: '1:1',
+          },
+        ],
+        tables: [
+          {
+            id: '1',
+            fields: [
+              {
+                id: '1',
+                name: 'test name',
+                PK: true,
+                type: 'objectId',
+              },
+            ],
+            tableName: 'test table name',
+            x: 15,
+            y: 25,
           },
         ],
       };
