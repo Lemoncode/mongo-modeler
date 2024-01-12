f4e08c179b44aa27d6e871439c2549ca2cc49a68
Tony <shidadk@hotmail.es>
2024-01-12 14:06:34 +0100

    Add unit tests in mapTableVmToEditTableVm and mapEditTableVmToTableVm

---
+++ src/pods/edit-table/edit-table.mapper.spec.ts
@@ -0,0 +1,185 @@
+import {
+  mapEditTableVmToTableVm,
+  mapTableVmToEditTableVm,
+} from './edit-table.mapper';
+import * as canvasModel from '@/core/providers/canvas-schema/canvas-schema.model';
+import * as editTableModel from './edit-table.vm';
+
+describe('mapEditTableVmToTableVm', () => {
+  it('should return a copy of "canvasTable" witout "fields"', () => {
+    // Arrange
+    const table: editTableModel.TableVm = {
+      id: '1',
+      fields: [],
+      tableName: 'ProductA',
+      x: 2,
+      y: 3,
+    };
+    const expectedResult: canvasModel.TableVm = {
+      id: '1',
+      fields: [],
+      tableName: 'ProductA',
+      x: 2,
+      y: 3,
+    };
+    // Act
+    const result = mapEditTableVmToTableVm(table);
+    // Assert
+    expect(result).toEqual(expectedResult);
+  });
+
+  describe('mapEditTableFieldsToTableVmFields', () => {
+    it('should return a canvasVm correcly', () => {
+      // Arrange
+      const table: editTableModel.TableVm = {
+        id: '01',
+        fields: [
+          {
+            id: '1',
+            PK: false,
+            FK: false,
+            name: '_id',
+            type: 'objectId',
+            children: [
+              {
+                id: '3',
+                FK: false,
+                PK: false,
+                name: '_type',
+                type: 'string',
+                children: [],
+              },
+            ],
+          },
+          {
+            id: '2',
+            PK: false,
+            FK: false,
+            name: '_name',
+            type: 'string',
+          },
+        ],
+        tableName: 'ProductA',
+        x: 2,
+        y: 3,
+      };
+      const expectedResult: canvasModel.TableVm = {
+        id: '01',
+        fields: [
+          {
+            id: '1',
+            PK: false,
+            name: '_id',
+            type: 'objectId',
+            children: [
+              {
+                id: '3',
+                PK: false,
+                name: '_type',
+                type: 'string',
+                children: [],
+              },
+            ],
+          },
+          {
+            id: '2',
+            PK: false,
+            name: '_name',
+            type: 'string',
+            children: [],
+          },
+        ],
+        tableName: 'ProductA',
+        x: 2,
+        y: 3,
+      };
+      // Act
+      const result = mapEditTableVmToTableVm(table);
+      // Assert
+      expect(result).toEqual(expectedResult);
+    });
+  });
+});
+
+describe('mapTableVmToEditTableVm', () => {
+  it('should return a editTableVm with the correct FK  ', () => {
+    // Arrange
+    const table: canvasModel.TableVm = {
+      id: '01',
+      fields: [
+        {
+          id: '1',
+          PK: false,
+          name: '_id',
+          type: 'objectId',
+          children: [
+            {
+              id: '3',
+              PK: false,
+              name: '_type',
+              type: 'string',
+              children: [],
+            },
+          ],
+        },
+        {
+          id: '2',
+          PK: false,
+          name: '_name',
+          type: 'string',
+          children: [],
+        },
+      ],
+      tableName: 'ProductA',
+      x: 2,
+      y: 3,
+    };
+    const relation: canvasModel.RelationVm[] = [
+      {
+        fromTableId: '01',
+        toTableId: '02',
+        fromFieldId: '1',
+        toFieldId: '2',
+        type: '1:1',
+      },
+    ];
+
+    const expectedResult: editTableModel.TableVm = {
+      id: '01',
+      fields: [
+        {
+          id: '1',
+          PK: false,
+          FK: true,
+          name: '_id',
+          type: 'objectId',
+          children: [
+            {
+              id: '3',
+              FK: false,
+              PK: false,
+              name: '_type',
+              type: 'string',
+              children: [],
+            },
+          ],
+        },
+        {
+          id: '2',
+          PK: false,
+          FK: false,
+          name: '_name',
+          type: 'string',
+          children: [],
+        },
+      ],
+      tableName: 'ProductA',
+      x: 2,
+      y: 3,
+    };
+    // Act
+    const result = mapTableVmToEditTableVm(table, relation);
+    // Assert
+    expect(result).toEqual(expectedResult);
+  });
+});
