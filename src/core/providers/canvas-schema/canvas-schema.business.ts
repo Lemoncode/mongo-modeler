2fad346cc71333b728d2813baa3763b2b94ed8ad
Braulio Diez Botella <braulio.diez@gmail.com>
2024-01-13 12:32:12 +0100

    #79 closes

--- src/core/providers/canvas-schema/canvas-schema.business.ts
+++ src/core/providers/canvas-schema/canvas-schema.business.ts
@@ -1,6 +1,69 @@
 import { produce } from 'immer';
-import { TableVm } from './canvas-schema.model';
+import { FieldVm, RelationVm, TableVm } from './canvas-schema.model';
 import { DatabaseSchemaVm } from './canvas-schema.model';
+import { GUID } from '@/core/model';
+
+const getFieldIdCollectionFromTable = (table: TableVm): GUID[] => {
+  const fieldIds: GUID[] = [];
+
+  const collectFieldIds = (field: FieldVm) => {
+    fieldIds.push(field.id);
+    if (field.children) {
+      field.children.forEach(collectFieldIds);
+    }
+  };
+
+  table.fields.forEach(collectFieldIds);
+  return fieldIds;
+};
+
+const extractDeletedFields = (
+  originalFieldIds: GUID[],
+  newFieldIds: GUID[]
+): GUID[] => {
+  const deletedFieldIdsCollection: GUID[] = [];
+
+  originalFieldIds.forEach(originalFieldId => {
+    if (newFieldIds.find(id => id === originalFieldId) === undefined) {
+      deletedFieldIdsCollection.push(originalFieldId);
+    }
+  });
+
+  return deletedFieldIdsCollection;
+};
+
+const removeRelationsThatContainsDeletedFields = (
+  relationCollection: RelationVm[],
+  fieldIdsDeleted: GUID[]
+): RelationVm[] =>
+  relationCollection.filter(
+    relation =>
+      fieldIdsDeleted.find(id => id === relation.fromFieldId) === undefined &&
+      fieldIdsDeleted.find(id => id === relation.toFieldId) === undefined
+  );
+
+interface TablesInfo {
+  originTable: TableVm;
+  updatedTable: TableVm;
+}
+
+const pruneOrphanRelationsLogic = (
+  tablesInfo: TablesInfo,
+  relations: RelationVm[]
+): RelationVm[] => {
+  const { originTable, updatedTable } = tablesInfo;
+  const originFieldIds = getFieldIdCollectionFromTable(originTable);
+  const updatedFieldIds = getFieldIdCollectionFromTable(updatedTable);
+
+  // 1. Extract Deleted Fields
+  const deletedFields = extractDeletedFields(originFieldIds, updatedFieldIds);
+
+  // 2. Remove Relations that contains deleted fields
+  const relationsWithoutDeletedFields =
+    removeRelationsThatContainsDeletedFields(relations, deletedFields);
+
+  return relationsWithoutDeletedFields;
+};
 
 export const updateTable = (
   table: TableVm,
@@ -9,6 +72,13 @@ export const updateTable = (
   produce(dbSchema, draft => {
     const tableIndex = draft.tables.findIndex(t => t.id === table.id);
     if (tableIndex !== -1) {
+      // Prune orphan relations
+      draft.relations = pruneOrphanRelationsLogic(
+        { originTable: draft.tables[tableIndex], updatedTable: table },
+        draft.relations
+      );
+
+      // Update table content with new values
       draft.tables[tableIndex] = table;
     }
   });
