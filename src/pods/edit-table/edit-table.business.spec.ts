fdb8c8bd9d34c3fb305c2ad05670de88896a646b
AlbertoSSC <albertossc@gmail.com>
2024-01-16 18:15:43 +0100

    refactor & test: divided into two functions the component and added testing to recursive function removeFieldRecursive

--- src/pods/edit-table/edit-table.business.spec.ts
+++ src/pods/edit-table/edit-table.business.spec.ts
@@ -1,6 +1,9 @@
-import { removeField } from './edit-table.business';
-import { TableVm } from './edit-table.vm';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { produce } from 'immer';
+import { removeField, removeFieldRecursive } from './edit-table.business';
+import { FieldVm, TableVm } from './edit-table.vm';
 import { GUID } from '@/core/model';
+import { vi } from 'vitest';
 
 const table: TableVm = {
   id: '1',
@@ -39,6 +42,40 @@ const table: TableVm = {
   x: 0,
   y: 0,
 };
+
+beforeEach(() => {
+  vi.clearAllMocks();
+});
+
+vi.mock('./edit-table.business', async () => {
+  const actual = await vi.importActual('./edit-table.business');
+
+  const mockedRemoveField = (table: TableVm, fieldId: GUID): TableVm => {
+    return produce(table, draftTable => {
+      mockedRemoveFieldRecursive(draftTable.fields, fieldId);
+    });
+  };
+
+  const mockedRemoveFieldRecursive = vi.fn(
+    (fields: FieldVm[], fieldId: GUID): void => {
+      for (let i = 0; i < fields.length; i++) {
+        if (fields[i].id === fieldId) {
+          fields.splice(i, 1);
+          return; // Exit early if the field is found and has been removed
+        }
+        if (fields[i].children) {
+          mockedRemoveFieldRecursive(fields[i].children ?? [], fieldId);
+        }
+      }
+    }
+  );
+
+  return {
+    ...actual,
+    removeField: mockedRemoveField,
+    removeFieldRecursive: mockedRemoveFieldRecursive,
+  };
+});
 
 describe('edit table - removeField function', () => {
   it('should remove the specified field and its children from the table ', () => {
@@ -134,5 +171,17 @@ describe('edit table - removeField function', () => {
     expect(result.fields.some(field => field.id === fieldIdToRemove)).toBe(
       false
     );
+  });
+
+  it('should call removeFieldRecursive when field has children', async () => {
+    // Arrange
+    const fieldIdToRemove: GUID = '3';
+
+    // Act
+    removeField(table, fieldIdToRemove);
+
+    // Assert
+    expect(removeFieldRecursive).toHaveBeenCalled();
+    expect(removeFieldRecursive).toHaveBeenCalledTimes(3); //the first for id=1, the second for id=2 (has children) then a third time recursively
   });
 });
