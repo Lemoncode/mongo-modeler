e16f4e177929ce3b1fd3fdb521279331f381852b
AlbertoSSC <albertossc@gmail.com>
2024-01-18 20:01:02 +0100

    Feature: added to local storage user theme preference

--- src/core/providers/theme-provider/theme-provider.tsx
+++ src/core/providers/theme-provider/theme-provider.tsx
@@ -5,28 +5,46 @@ import { ThemeModel, createInitialTheme } from './theme.model';
 interface Props {
   children: React.ReactNode;
 }
-export const ThemeProvider: React.FC<Props> = (props) => {
+
+export const ThemeProvider: React.FC<Props> = props => {
   const { children } = props;
   const [theme, setTheme] = React.useState<ThemeModel>(createInitialTheme());
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => ({
+    localStorage.setItem(
+      'themeMode',
+      theme.themeMode === 'light' ? 'dark' : 'light'
+    );
+
+    setTheme(prevTheme => ({
       ...prevTheme,
       themeMode: prevTheme.themeMode === 'light' ? 'dark' : 'light',
     }));
   };
 
+  React.useEffect(() => {
+    const themeMode = localStorage.getItem('themeMode');
+    if (themeMode) {
+      setTheme(prevTheme => ({
+        ...prevTheme,
+        themeMode: themeMode as 'dark' | 'light',
+      }));
+    }
+  }, []);
+
   return (
-    <ThemeContext.Provider value={{theme, toggleTheme}}>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
-  )
-}
+  );
+};
 
 export const useThemeContext = () => {
   const context = React.useContext(ThemeContext);
   if (context === null) {
-    throw new Error('useThemeContext: Ensure you have wrapped your app with ThemeProvider');
+    throw new Error(
+      'useThemeContext: Ensure you have wrapped your app with ThemeProvider'
+    );
   }
 
   return context;
