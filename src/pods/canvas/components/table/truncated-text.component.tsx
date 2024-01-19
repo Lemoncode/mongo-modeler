10f19c71a3f3642a316b55ab515eed1282fed26a
Marcos Apodaca <marcosapodaca@outlook.es>
2024-01-19 18:27:14 +0100

    Correction of the text overlapping

---
+++ src/pods/canvas/components/table/truncated-text.component.tsx
@@ -0,0 +1,32 @@
+import { GUID } from '@/core/model';
+import classes from './database-table.module.css';
+
+interface Props {
+  id: GUID;
+  text: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export const TruncatedText: React.FC<Props> = props => {
+  const { text, x, y, width, height, id } = props;
+
+  return (
+    <>
+      <clipPath id={`clip_${id}`}>
+        <rect x={x} y={y} width={width} height={height + 10}></rect>
+      </clipPath>
+
+      <text
+        x={x + 5}
+        y={y + height}
+        clip-path={`url(#clip_${id})`}
+        className={classes.tableTextRow}
+      >
+        {text}
+      </text>
+    </>
+  );
+};
