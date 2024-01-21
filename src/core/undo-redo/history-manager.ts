37fc26b152eb6bc47ca73a05b8798b5936692eb4
Braulio Diez Botella <braulio.diez@gmail.com>
2024-01-21 17:49:35 +0100

    Refactor history manager to limit the number of snapshots

--- src/core/undo-redo/history-manager.ts
+++ src/core/undo-redo/history-manager.ts
@@ -1,44 +1,55 @@
-import { useState, useCallback, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { DatabaseSchemaVm } from '../providers';
+
+const MAX_HISTORY_LENGTH = 20; // Maximum limit for the history
 
 export const useHistoryManager = (initialState: DatabaseSchemaVm) => {
   const [canvasSchema, setCanvasSchema] =
     useState<DatabaseSchemaVm>(initialState);
 
-  // useRef para mantener el historial y el índice actual sin provocar renderizaciones
+  // useRef to hold the historic and the actual index and avoid issues with async updates
   const historyRef = useRef<DatabaseSchemaVm[]>([initialState]);
   const currentIndexRef = useRef<number>(0);
 
   const addSnapshot = (newSchema: DatabaseSchemaVm) => {
-    const nextIndex = currentIndexRef.current + 1;
-    const currentHistory = historyRef.current.slice(0, nextIndex);
+    const nextIndex = Math.min(
+      currentIndexRef.current + 1,
+      MAX_HISTORY_LENGTH - 1
+    );
+    let currentHistory = historyRef.current.slice(0, nextIndex);
+
+    if (currentHistory.length >= MAX_HISTORY_LENGTH) {
+      // Remove the first element if the history exceeds the maximum size
+      currentHistory = currentHistory.slice(1);
+    }
+
     historyRef.current = [...currentHistory, newSchema];
-    currentIndexRef.current = nextIndex;
+    currentIndexRef.current = historyRef.current.length - 1;
   };
 
-  const undo = useCallback(() => {
+  const undo = () => {
     const prevIndex = currentIndexRef.current - 1;
     if (prevIndex >= 0) {
       currentIndexRef.current = prevIndex;
       setCanvasSchema(historyRef.current[prevIndex]);
     }
-  }, []);
+  };
 
-  const redo = useCallback(() => {
+  const redo = () => {
     const nextIndex = currentIndexRef.current + 1;
     if (nextIndex < historyRef.current.length) {
       currentIndexRef.current = nextIndex;
       setCanvasSchema(historyRef.current[nextIndex]);
     }
-  }, []);
+  };
 
-  const canUndo = useCallback(() => {
+  const canUndo = () => {
     return currentIndexRef.current > 0;
-  }, []);
+  };
 
-  const canRedo = useCallback(() => {
+  const canRedo = () => {
     return currentIndexRef.current < historyRef.current.length - 1;
-  }, []);
+  };
 
   const getCurrentState = () => historyRef.current[currentIndexRef.current];
 
