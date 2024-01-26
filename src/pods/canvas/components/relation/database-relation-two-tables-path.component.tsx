cd64dbd140a254dcddfb07083fdca0afca6a3deb
manudous <manudous@gmail.com>
2024-01-26 14:59:42 +0100

    add oneToOneRelationSelfPathLeft in DatabaseRelationshipTwoTablePathComponent

--- src/pods/canvas/components/relation/database-relation-two-tables-path.component.tsx
+++ src/pods/canvas/components/relation/database-relation-two-tables-path.component.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { RelationType } from '@/core/providers/canvas-schema';
+import { RelationType, TABLE_CONST } from '@/core/providers/canvas-schema';
 import { Coords, GUID } from '@/core/model';
-import { DatabaseRelationshipTwoTablesStraightComponent } from './database-relation-two-tables-straight.component';
-
+import classes from './database-relation.component.module.css';
+import { ClickableLineComponent } from './components';
 interface DatabaseRelationshipMiddleProps {
   id: GUID;
   relationType: RelationType;
@@ -17,24 +17,62 @@ export const DatabaseRelationshipTwoTablePathComponent: React.FC<
   DatabaseRelationshipMiddleProps
 > = ({
   id,
-  relationType,
   startCoords,
+  relationType,
   endCoords,
   isSelected,
   onClick,
   onDoubleClick,
 }) => {
-  console.log('DRAW PATH CONNECTOR');
+  const oneToOneRelationSelfPathLeft = `
+  M ${startCoords.x - TABLE_CONST.TABLE_WIDTH} ${startCoords.y} 
+  H ${startCoords.x - (TABLE_CONST.TABLE_WIDTH + TABLE_CONST.HORIZONTAL_LEFT_EXTENSION)} 
+  V ${endCoords.y} 
+  H ${endCoords.x}
+  `;
+
+  const oneToOneRelationSelfPathRight = `
+  M ${startCoords.x + TABLE_CONST.TABLE_WIDTH} ${startCoords.y}
+  H ${startCoords.x + TABLE_CONST.TABLE_WIDTH + TABLE_CONST.HORIZONTAL_LEFT_EXTENSION}
+  V ${endCoords.y}
+  H ${endCoords.x}
+  `;
+
+  const getRelationPath = () => {
+    if (relationType === '1:1') {
+      return startCoords.x < endCoords.x
+        ? oneToOneRelationSelfPathRight
+        : oneToOneRelationSelfPathLeft;
+    }
+  };
 
   return (
-    <DatabaseRelationshipTwoTablesStraightComponent
-      id={id}
-      onClick={onClick}
-      onDoubleClick={onDoubleClick}
-      relationType={relationType}
-      startCoords={startCoords}
-      endCoords={endCoords}
-      isSelected={isSelected}
-    />
+    <svg>
+      {/* Glow filter if selected */}
+      <defs>
+        <filter id="table_glow">
+          <feGaussianBlur stdDeviation="3.5" result="coloredBlur" />
+          <feMerge>
+            <feMergeNode in="coloredBlur" />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+      </defs>
+      <path
+        d={getRelationPath()}
+        className={
+          isSelected ? classes.selectedRelation : classes.nonSelectedRelation
+        }
+        filter={isSelected ? `url(#table_glow)` : ''}
+      />
+
+      <ClickableLineComponent
+        id={id}
+        startCoords={startCoords}
+        endCoords={endCoords}
+        onClick={onClick}
+        onDoubleClick={onDoubleClick}
+      />
+    </svg>
   );
 };
