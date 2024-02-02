c9cbf99ff667836bfa523745b3f7a2e6851b44cd
deletidev <deletidev@gmail.com>
2024-02-02 13:55:00 +0100

    working

--- src/pods/about/about.pod.tsx
+++ src/pods/about/about.pod.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export const AboutPod: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Mongo Modeler</h2>
+      <p>Version 0.1</p>
+      <p>Community preview</p>
+      <p>Github</p>
+
+      <h2>Team</h2>
+      <div className="member">
+        <div className="member-image">
+          <img src="" alt="" />
+          <a href="">LinkIn</a>
+        </div>
+        <p className="member-name">Name</p>
+      </div>
+    </div>
+  );
 };
