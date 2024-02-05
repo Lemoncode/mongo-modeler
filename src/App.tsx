c197f219b96f6c0926ebf0bd9901109b3955ff24
deletidev <deletidev@gmail.com>
2024-02-05 18:00:38 +0100

    working on mobile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-
-import "./App.css";
-import { MainScene } from "@/scenes";
-import { useThemeContext } from "./core/providers";
+import React from 'react';
+import { MainScene, MobileTabletScene } from '@/scenes';
+import { useThemeContext } from './core/providers';
+import './App.css';
 
 function App() {
-  const { theme } = useThemeContext()
+  const { theme } = useThemeContext();
+  const [width, setWidth] = React.useState(true);
+
+  const handleResize = () => {
+    window.innerWidth < 1160 ? setWidth(false) : setWidth(true);
+  };
+
+  React.useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className={ theme.themeMode }>
-      <MainScene />
+    <div className={theme.themeMode}>
+      {width ? <MainScene /> : <MobileTabletScene />}
     </div>
   );
 }
---
+++ src/scenes/mobile-tablet.scene.tsx
@@ -0,0 +1,9 @@
+import classes from './main.scene.module.css';
+
+export const MobileTabletScene: React.FC = () => {
+  return (
+    <div className={classes.container}>
+      <img src="/assets/team-work-cuate.svg" alt="" />
+    </div>
+  );
+};
