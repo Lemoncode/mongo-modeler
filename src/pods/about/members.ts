de0cff4bdd1d540def66021a7903126000981fac
deletidev <deletidev@gmail.com>
2024-02-05 11:58:29 +0100

    members

--- src/pods/about/members.ts
+++ src/pods/about/members.ts
@@ -1,5 +1,3 @@
-import leticia from '/members/leticia.jpeg';
-
 export interface Member {
   id: string;
   name: string;
@@ -10,26 +8,44 @@ export interface Member {
 export const memberList: Member[] = [
   {
     id: '1',
-    name: 'Leticia de la Osa',
-    urlLinkedin: 'https://www.linkedin.com/in/deletidev/',
-    image: leticia,
+    name: 'Teresa Milanés',
+    urlLinkedin: 'https://www.linkedin.com/in/mteresamb/',
+    image: './assets/teresa-milanes.jpeg',
   },
   {
     id: '2',
-    name: 'Leticia de la Osa',
-    urlLinkedin: 'https://www.linkedin.com/in/deletidev/',
-    image: leticia,
+    name: 'Tony Torres',
+    urlLinkedin: '',
+    image: './assets/tony-torres.jpeg',
   },
   {
     id: '3',
-    name: 'Leticia de la Osa',
-    urlLinkedin: 'https://www.linkedin.com/in/deletidev/',
-    image: leticia,
+    name: 'Alberto Santiago',
+    urlLinkedin: 'https://www.linkedin.com/in/albertos2c/',
+    image: './assets/alberto-santiago.png',
   },
   {
     id: '4',
+    name: 'Verónica Camarzana',
+    urlLinkedin: 'https://www.linkedin.com/in/veronicacamarzana/',
+    image: './assets/veronica-camarzana.jpeg',
+  },
+  {
+    id: '5',
+    name: 'Marcos Apocada',
+    urlLinkedin: 'https://www.linkedin.com/in/marcos-apodaca/',
+    image: './assets/marcos-apocada.jpeg',
+  },
+  {
+    id: '6',
+    name: 'Abel de Tena',
+    urlLinkedin: 'https://www.linkedin.com/in/abeldetena/',
+    image: './assets/abel-de-tena.jpeg',
+  },
+  {
+    id: '7',
     name: 'Leticia de la Osa',
     urlLinkedin: 'https://www.linkedin.com/in/deletidev/',
-    image: leticia,
+    image: './assets/leticia-de-la-osa.jpeg',
   },
 ];
