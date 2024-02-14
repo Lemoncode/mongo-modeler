d0cc4df9a7e4b4504fa708c03f0465cd13f2252d
manudous <manudous@gmail.com>
2024-02-14 20:29:10 +0100

    modify to 20 attemps

--- src/core/providers/canvas-schema/canvas.const.ts
+++ src/core/providers/canvas-schema/canvas.const.ts
@@ -11,7 +11,7 @@ const ROW_HEIGHT = FONT_SIZE + ROW_PADDING; // Height of the table line
 const HORIZONTAL_LEFT_EXTENSION = 20; // Horizontal extension for the relation line 1:1
 const HEADER_TITLE_GAP = 15;
 const CANVAS_PADDING = 100; // Represents the additional space around the canvas, we use it to add a space for the tables when we export the schema
-const MAX_PLACEMENT_ATTEMPTS = 10; // Maximum number of attempts to place a table without overlapping
+const MAX_PLACEMENT_ATTEMPTS = 20; // Maximum number of attempts to place a table without overlapping
 const TABLE_SHIFT_DISTANCE = 30; // Distance to shift the table when it overlaps with another table
 
 export const TABLE_CONST = {
