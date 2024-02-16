7abb6fb469669340dd49f7d81b9eec7b64076ae0
deletidev <deletidev@gmail.com>
2024-02-16 14:41:16 +0100

    move the input into a folder

--- src/common/components/forms/index.ts
+++ src/common/components/forms/index.ts
@@ -1,4 +1,4 @@
-export * from './input-formik.component';
+export * from './input-formik';
 export * from './dropdown-formik';
 export * from './table-pk-picker-formik';
 export * from './checkbox-formik';
