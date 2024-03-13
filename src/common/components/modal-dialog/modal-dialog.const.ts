246d457a8d246ed6b868fd558ac9370166e0ba59
IonutGabi <birsanionut@gmail.com>
2024-03-13 13:20:23 +0100

    Fixed modal title when clicking the Add Relation button

--- src/common/components/modal-dialog/modal-dialog.const.ts
+++ src/common/components/modal-dialog/modal-dialog.const.ts
@@ -1,4 +1,5 @@
 export const CANVAS_SETTINGS_TITLE = 'Canvas Settings';
+export const ADD_RELATION_TITLE = 'Add relation';
 export const EDIT_RELATION_TITLE = 'Edit relation';
 export const ADD_COLLECTION_TITLE = 'Add Collection';
 export const EDIT_COLLECTION_TITLE = 'Edit Collection';
