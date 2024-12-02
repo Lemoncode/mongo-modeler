a8789c9331d1d51edbeac2b0c34797c39a000155
Fran Lopez <franciscolopto@gmail.com>
2024-12-02 11:48:06 +0100

    add url in webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -54,6 +54,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
+    url: BASE_URL,
     reuseExistingServer: !process.env.CI,
   },
 });
