01cee40d6054f693b19ad2a596810da4eb29a7e1
oleojake <oleojake@gmail.com>
2025-03-19 19:33:08 +0100

    #513-add-textarea-to-modal

--- src/pods/import-collection/import-panel.pod.tsx
+++ src/pods/import-collection/import-panel.pod.tsx
@@ -12,14 +12,42 @@ interface ImportPanelProps {
   onClose: () => void;
 }
 
+const defaultJson = JSON.stringify(
+  {
+    _id: { $oid: '5f5f9b3b4b4b1f001f1b1f1b' },
+    age: '30',
+    name: 'John Doe',
+  },
+  null,
+  2
+);
+
 export const ImportPanel: React.FC<ImportPanelProps> = props => {
   const { onSave, onClose, relations, table } = props;
+  const [jsonContent, setJsonContent] = React.useState<string>(defaultJson);
+  const [jsonError, setJsonError] = React.useState<string | null>(null);
+
   const [editTable, setEditTable] = React.useState<editTableVm.TableVm>(() =>
     doMapOrCreateTable(relations, table)
   );
 
   const handleSubmit = (table: editTableVm.TableVm) => {
+    console.log(jsonContent);
     onSave(mapEditTableVmToTableVm(table));
+  };
+
+  const handleJsonContentChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const newValue = e.currentTarget.value;
+    setJsonContent(newValue);
+
+    try {
+      JSON.parse(newValue);
+      setJsonError(null);
+    } catch (error) {
+      setJsonError('El JSON no es válido');
+    }
   };
 
   const handleChangeTableName = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,10 +68,24 @@ export const ImportPanel: React.FC<ImportPanelProps> = props => {
           />
         </label>
       </div>
+      <div className={classes.jsonTextarea}>
+        <label>
+          JSON Schema:
+          <textarea
+            value={jsonContent}
+            onChange={handleJsonContentChange}
+            onFocus={e => e.currentTarget.select()}
+          />
+        </label>
+      </div>
+      <div className={classes.errorMessage} style={{ minHeight: '20px' }}>
+        {jsonError && <p style={{ color: 'red', margin: 0 }}>{jsonError}</p>}
+      </div>
       <div className="two-buttons">
         <button
           className="button-secondary"
           onClick={() => handleSubmit(editTable)}
+          disabled={jsonError !== null}
         >
           Apply
         </button>
