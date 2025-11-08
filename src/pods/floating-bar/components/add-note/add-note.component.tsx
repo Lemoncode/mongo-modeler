e1a5701b606fe2373a7a5c098c5155cfa85b281e
Guste Gaubaite <219.guste@gmail.com>
2025-11-08 10:39:07 +0100

    feat(#561 add-note): implement AddNote functionality with modal integration for note creation

--- src/pods/floating-bar/components/add-note/add-note.component.tsx
+++ src/pods/floating-bar/components/add-note/add-note.component.tsx
@@ -1,12 +1,40 @@
 import { ActionButton } from '@/common/components/action-button';
 import classes from '../floating-bar-components.module.css';
 import { NoteIcon } from '@/common/components/icons/note-icon.component';
+import {
+  NoteVm,
+  useCanvasSchemaContext,
+  useCanvasViewSettingsContext,
+  useModalDialogContext,
+} from '@/core/providers';
+import { EditNotePod } from '@/pods/edit-note';
+import { ADD_NOTE_TITLE } from '@/common/components';
+
+const BORDER_MARGIN = 40;
 
 export const AddNote = () => {
-  const handleAddNoteClick = () => {
-    console.log('Modal open');
+  const { openModal, closeModal } = useModalDialogContext();
+  const { addNote } = useCanvasSchemaContext();
+  const { canvasViewSettings, setLoadSample } = useCanvasViewSettingsContext();
+
+  const handleAddNote = (newNote: NoteVm) => {
+    const updatedNote = {
+      ...newNote,
+      x: canvasViewSettings.scrollPosition.x + BORDER_MARGIN,
+      y: canvasViewSettings.scrollPosition.y + BORDER_MARGIN,
+    };
+
+    addNote(updatedNote);
+    closeModal();
   };
 
+  const handleAddNoteClick = () => {
+    setLoadSample(false);
+    openModal(
+      <EditNotePod onSave={handleAddNote} onClose={closeModal} />,
+      ADD_NOTE_TITLE
+    );
+  };
   return (
     <ActionButton
       icon={<NoteIcon />}
