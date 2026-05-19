605e70bc0bf56e57ba15d666a0282218933dfa56
Ivanruii <ivr.644@gmail.com>
2026-05-19 11:55:00 +0200

    feat(vscode-extension): add status bar item for new Mongo Modeler diagram

--- packages/vscode-extension/src/index.ts
+++ packages/vscode-extension/src/index.ts
@@ -1,10 +1,12 @@
 import { registerCommands } from '#commands';
 import { MongoModelerEditorProvider } from '#editor';
+import { registerStatusBarItems } from '#status-bar';
 import * as vscode from 'vscode';
 
 export const activate = (context: vscode.ExtensionContext) => {
   context.subscriptions.push(MongoModelerEditorProvider.register(context));
   registerCommands(context);
+  registerStatusBarItems(context);
 };
 
 export const deactivate = () => {};
