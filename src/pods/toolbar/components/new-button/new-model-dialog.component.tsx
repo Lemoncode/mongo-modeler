75a1a8bb03d4ad7c5e8100f1d0078a4c4a8ccc59
nachodiezgarcia <diezgarcianacho@gmail.com>
2026-06-11 17:46:27 +0200

    feature: adding some style classes for the confirmation dialog

--- src/pods/toolbar/components/new-button/new-model-dialog.component.tsx
+++ src/pods/toolbar/components/new-button/new-model-dialog.component.tsx
@@ -1,3 +1,5 @@
+import classes from './new-model.module.css'
+
 interface Props {
     onConfirm: () => void;
     onCancel: () => void;
@@ -5,17 +7,17 @@ interface Props {
 
 export const NewModelDialog = ({ onConfirm, onCancel }: Props) => (
   <div role="alertdialog" aria-modal="true">
-    <p>You have unsaved changes. Are you sure you want to create a new model? All current progress will be lost.</p>
+    <p className={classes.menssage}>You have unsaved changes. Are you sure you want to create a new model? All current progress will be lost.</p>
     <div className="two-buttons" >
         <button
             className="button-secondary"
-            onClick={onCancel}>
-            Cancel
+            onClick={onConfirm}>
+            Confirm
         </button>
         <button
             className="button-tertiary"
-            onClick={onConfirm}>
-            Confirm
+            onClick={onCancel}>
+            Cancel
         </button>
     </div>
   </div>
